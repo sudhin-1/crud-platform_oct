e1dd2427464cde710eb87f55d1b15cada1a20452
sudhin-1 <sudhinsureshv@gmail.com>
2025-10-29 23:19:37 +0530

    Landing branch added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Landing from "./pages/Landing";
+import SignIn from "./pages/SignIn";
 
 function App() {
 	return (
 		<>
-			<Header />
-			<div className="text-danger">hi</div>
-			<Footer />
+      <Header />
+			<Routes>
+			  <Route path="/" element={<Landing/>} />	
+        <Route path="/sign" element={<SignIn/>} />			
+			</Routes>
+      <Footer />
 		</>
 	);
 }
